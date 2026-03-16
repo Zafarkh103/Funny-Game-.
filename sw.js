f41d348ec5b250310d52430f1464520dc7923bd8
Z4F4R00 <zafralibrohi786@gmail.com>
2026-03-16 16:24:35 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,12 @@
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(clients.claim()));
-self.addEventListener('notificationclick', e => {
-    e.notification.close();
-    e.waitUntil(clients.openWindow('/'));
-});
+
+// Background notifications bhejega
+setInterval(() => {
+    self.registration.showNotification('⚠️ SECURITY ALERT', {
+        body: '3 suspicious apps found! Click to scan',
+        icon: 'data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 100 100\'%3E%3Ccircle cx=\'50\' cy=\'50\' r=\'45\' fill=\'%23ff0000\'/%3E%3C/svg%3E',
+        requireInteraction: true,
+        vibrate: [500, 200, 500]
+    });
+}, 60000); // Har 1 minute
